7fb000b7ac260f6d51591f5a784fd3538a6fd7d0
William Jackson <lila.lilajackson@hotmail.com>
2026-02-02 11:14:48 -0300

    fixing hero's css

--- src/sections/Hero.styles.js
+++ src/sections/Hero.styles.js
@@ -2,9 +2,16 @@ import styled from "styled-components";
 
 export const HeroSection = styled.section`
     display: flex;
-    justify-content: center;
     align-items: center;
-    margin: 100px 100px;
+    gap: 20px;
+    height: 100vh;
+    width: 75vw;
+    margin: 0 auto;
+
+    @media(max-width: 768px) {
+        display:flex;
+        flex-direction: column-reverse;
+    }
     
 `
 
@@ -20,15 +27,19 @@ export const HeroTitle = styled.h1`
     margin-bottom: 12px;
 `
 
-export const HeroSubTitle = styled.h1`
+export const HeroSubTitle = styled.h3`
     font-size: 14px;
     line-height: 26px;
     font-weight: 200;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    letter-spacing: 0.02rem;
+    
 `
 
 export const HeroImage = styled.img`
-  width: 300px;
-  height: 300px;
+  width: 400px;
+  height: 400px;
   border-radius: 50%;
   object-fit: cover;
 `
